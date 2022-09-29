2a199ec03432c45311871cd544de4c33ebdec193
cyccc95 <cyccc95@naver.com>
2022-09-29 17:11:57 +0900

    Add javascript comment

--- javascript/js02_1.js
+++ javascript/js02_1.js
@@ -12,13 +12,11 @@
 // 함수는 const로만, let x
 
 
-// 랜덤 주사위 굴리기
-// let randomNum = Math.floor(Math.random()*6)+1;
-
-//   newDiv = `<div id="diceImg"><img src="../image/${randomNum}.png"></div>`
-//   msgId.innerHTML = newDiv;
 
 
+//-------------주사위 게임 만들기-----------------//
+
+// 필요한 공통 변수들 선언
 let btn;
 let formSection;
 let diceSection;
@@ -26,41 +24,20 @@ let answerSection;
 let count, win; // 판수, 이긴판
 let level; // 승률에 따른 레벨
 
-
-
 /** DOM 요소가 생성된 후에 실행 */
 document.addEventListener("DOMContentLoaded", () => {
   btn = document.querySelector('#btn');
   formSection = document.querySelector('#formSection');
   diceSection = document.querySelector('#diceSection');
-  answerSection = document.querySelector('#answerSection');
-  count = 0;
-  win = 0;
+  answerSection = document.querySelector('#answerSection'); // DOM 생성 이후 각 section 찾아줌
+  count = 0; // 판수 0으로 초기화
+  win = 0;   // 이긴판 0으로 초기화
 })
 
-const getAnswer = (event) => {
-  const value = event.target.value; // 선택한 눈
-
-  let randomNum = Math.floor(Math.random()*6)+1; // 1~6 랜덤 정수 생성
-  
-  formSection.style.display = 'none'; 
-  answerSection.style.display = 'flex';
-  diceSection.innerHTML = `<div><img src="../image/${randomNum}.png"></div>`;
-  // value와 랜덤 정수 비교해서 answer 출력
-  if(value == randomNum){
-    answerSection.innerHTML = `<div><img src="../image/o.png"></div>`;
-    count++;
-    win++;
-  } else {
-    answerSection.innerHTML = `<div><img src="../image/x.png"></div>`;
-    count++;
-  }
-  btn.innerHTML = '한번 더?';
-  
-}
-
+// 1번째 버튼에 달린 event
 const show = () => {
   
+  // 버튼 눌렀을 때 formSection에 넣어줄 html 만들기
   let form = ``;
   for(let i = 0; i < 6; i++){
     form += `
@@ -70,7 +47,8 @@ const show = () => {
     </div>
     `;
   }
-  // 처음 버튼 한번 누르면 나옴
+
+  // 처음 버튼 한번 누르면 실행
   btn.innerHTML = '고르셈'; 
   formSection.innerHTML = `<form>${form}</form>`;
 
@@ -79,18 +57,40 @@ const show = () => {
     formSection.style.display = 'flex';
     answerSection.style.display = 'none';
     diceSection.innerHTML = '';
-    
   }
   
 }
 
+// 숫자 골랐을 때 정답 출력하는 event
+const getAnswer = (e) => {
+  const value = e.target.value; // 선택한 눈의 value
+
+  let randomNum = Math.floor(Math.random()*6)+1; // 1~6 랜덤 정수 생성
+  
+  formSection.style.display = 'none'; 
+  answerSection.style.display = 'flex';
+  diceSection.innerHTML = `<div><img src="../image/${randomNum}.png"></div>`;
+  
+  // value와 랜덤 정수 비교해서 answer 출력
+  if(value == randomNum){
+    answerSection.innerHTML = `<div><img src="../image/o.png"></div>`;
+    count++; // 판수 +1
+    win++;   // 승수 +1
+  } else {
+    answerSection.innerHTML = `<div><img src="../image/x.png"></div>`;
+    count++; // 판수 +1
+  }
+
+  btn.innerHTML = '한번 더?';
+  
+}
 
 
-// 전적보기 함수
+// 2번째 버튼 전적보기 event
 const showRate = () => {
   const rateSection = document.querySelector('#rateSection');
 
-  // level 나누는 계산
+  // 승률에 따라 level 나누는 계산
   if(Math.round(win/count*100) >= 70){
     level = "오.. 고수";
   } else if(Math.round(win/count*100) >= 20 && Math.round(win/count*100) < 70) {
@@ -99,6 +99,7 @@ const showRate = () => {
     level = "ㅋㅋ.. 접으셈";
   }
 
+  // 출력할 html
   rateSection.innerHTML = `
     <div>
       ${win}승 ${count-win}패 ${Math.round(win/count*100)}% 
