289206d543d26be245a636b18805fe1f18855a78
cyccc95 <cyccc95@naver.com>
2022-10-04 16:11:13 +0900

    add comment

--- pj3/scripts/index.js
+++ pj3/scripts/index.js
@@ -3,9 +3,6 @@
 //DOM이 로드가 되면 반복문을 이용하여 [0,0,0,0,0,0,0,0,1]로 초기화
 let num = [];
 
-//박스를 선택한 순서를 기록하는 배열
-let selNum = [];
-
 //박스를 선택한 개수를 기록하는 변수
 let cnt = 0 ;
 
@@ -17,13 +14,12 @@ let randomNum;
 
 // 폭탄섞기 함수
 const boxShuffle = () => {
-    shuffleFlag = true;
-    document.querySelector('#msg').innerHTML = "";
-    randomNum = Math.floor(Math.random()*9); // 0 ~ 8
-    num[randomNum] = 1;
-    selNum = [];
-    cnt = 0;
-    for(let i = 0; i < 3; i++){
+    shuffleFlag = true; // 폭탄섞기 누르면 true
+    document.querySelector('#msg').innerHTML = ""; // 버튼 밑의 메세지 모두 지움
+    randomNum = Math.floor(Math.random()*9); // 0 ~ 8 랜덤수 생성
+    num[randomNum] = 1; // num 배열의 램덤수 index에 폭탄 생성
+    cnt = 0; // 몇개의 박스를 선택했는지 세는 cnt 초기화
+    for(let i = 0; i < 3; i++){ // 박스들 모두 초기화
         document.querySelectorAll('.boardRow')[i].innerHTML = `
             <div class="boardBox" id="box${3*i+1}" onclick="show(${3*i+1})">${3*i+1}</div>
             <div class="boardBox" id="box${3*i+2}" onclick="show(${3*i+2})">${3*i+2}</div>
@@ -36,24 +32,24 @@ const boxShuffle = () => {
 
 // 박스 선택 함수
 const show = (id) => {
-    if(shuffleFlag == false){
+    if(shuffleFlag == false){ // 폭탄섞기를 안했으면 실행
         document.querySelector('#msg').innerHTML = "폭탄섞기를 해주세요";
-    } else {
-        if(selNum[id-1] == null && cnt == 8){
-            document.querySelector(`#box${id}`).innerHTML = '<img src="./images/hart.png">';
-            document.querySelector('#msg').innerHTML = "성공";
+    } else { // 폭탄섞기 하고 나서 실행
+        if(cnt == 8){ // 선택한 박스가 8개이면 실행(성공한거임)
+            document.querySelector(`#box${id}`).innerHTML = '<img src="./images/hart.png">'; // 마지막 박스 이미지 하트
+            document.querySelector('#msg').innerHTML = "성공"; // 버튼 밑에 성공 메세지 출력
            
-        } else {
-            if(id == randomNum + 1){
-                document.querySelector(`#box${id}`).innerHTML = '<img src="./images/boom.png">';
-                document.querySelector('#msg').innerHTML = "실패";
-                for(let i = 0; i < 9; i++){
+        } else { // 8개를 성공하기 전까지 실행
+            if(id == randomNum + 1){ // 누른 박스가 폭탄이면
+                document.querySelector(`#box${id}`).innerHTML = '<img src="./images/boom.png">'; // 폭탄 이미지
+                document.querySelector('#msg').innerHTML = "실패"; // 버튼 밑에 실패 메세지 출력
+                for(let i = 0; i < 9; i++){ // 폭탄을 눌러서 실패하면 모든 박스의 onclick 이벤트 제거
                     document.querySelector(`#box${i+1}`).onclick = null;
                 }
-            } else{    
-                document.querySelector(`#box${id}`).innerHTML = '<img src="./images/hart.png">';
-                selNum[id-1] = 1;
-                cnt++;
+            } else{ // 누른 박스가 폭탄이 아니면
+                document.querySelector(`#box${id}`).innerHTML = '<img src="./images/hart.png">'; // 하트 이미지
+                document.querySelector(`#box${id}`).onclick = null; // 누른 박스의 onclick 이벤트 제거
+                cnt++; // 누른 박스 개수 세는 cnt +1
             }
         }
         
